de4d9e234f4072bbc97aafe3e25cceee00af1ee1
evankeith <evanmkeith@gmail.com>
2021-12-11 10:58:16 -0800

    1) Setup landing page HTML & Style

---
+++ js/landingEyeSpyApp.js
@@ -0,0 +1,8 @@
+background-image: url(https://images.unsplash.com/photo-1496664444929-8c75efb9546f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80);
+    background-size: cover;
+
+
+
+$('#accept').click(function(){
+    console.log('yes!')
+})
